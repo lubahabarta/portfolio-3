f69b58c2fedc57c9d720f520c286a91dc087f1b0
jindrahabarta <jindriskuv@email.cz>
2024-04-06 10:56:09 +0200

    added prettier

--- src/app/_components/Sections/Tech/Tech.tsx
+++ src/app/_components/Sections/Tech/Tech.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react'
 
 export default function Tech() {
-  return (
-    <section
-      id="technology-section"
-      data-scroll-section
-      className="h-device flex items-center justify-center"
-    >
-      fdsaf
-    </section>
-  );
+    return (
+        <section
+            id="technology-section"
+            data-scroll-section
+            className="h-device flex items-center justify-center"
+        >
+            fdsaf
+        </section>
+    )
 }
