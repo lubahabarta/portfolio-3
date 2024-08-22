f2d533f2971b2caaca39edfc8a5ba874d93e3281
lubahabarta <lubahabarta@gmail.com>
2024-08-22 20:25:23 +0200

    added Frame svg

--- src/app/_components/Sections/About/About.tsx
+++ src/app/_components/Sections/About/About.tsx
@@ -2,18 +2,20 @@
 
 import gsap from 'gsap'
 import React, { useEffect } from 'react'
+import Frame from './Frame'
 
 export default function About() {
     useEffect(() => {
-        gsap.to('#something', {
+        gsap.to('#frame', {
             scrollTrigger: {
                 trigger: '#about-section',
                 start: 'top bottom',
-                end: '200% top',
+                end: 'bottom top',
                 scrub: true,
-                markers: true,
+                // markers: true,
             },
-            x: '+=100',
+            paused: true,
+            rotate: '12deg',
             ease: 'none',
         })
     }, [])
@@ -21,14 +23,24 @@ export default function About() {
     return (
         <section
             id="about-section"
-            className="relative bg-stone-950
+            className="relative h-device bg-stone-950
                 before:content-[''] before:absolute before:-top-20 before:w-screen before:h-20 before:bg-gradient-to-t before:from-stone-950 before:to-transparent"
         >
-            <div className="h-device flex items-center justify-center overflow-hidden px-20">
-                <p id="something" className="h-40">
-                    About section
-                </p>
-            </div>
+            <article className="h-auto lg:h-device flex flex-col lg:flex-row justify-center items-center px-20">
+                <Frame id="frame" className="block flex-1 h-5/6 -rotate-12" />
+                <div className="flex-1 flex flex-col items-center">
+                    <p className="text-[2vw] uppercase text-justify w-[35vw]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Expedita fugit quasi quod quae optio repellendus
+                        necessitatibus corrupti officiis.
+                    </p>
+                    <p className="text-[2vw] uppercase text-justify w-[35vw] mt-[2vw]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Expedita fugit quasi quod quae optio repellendus
+                        necessitatibus corrupti officiis.
+                    </p>
+                </div>
+            </article>
         </section>
     )
 }
